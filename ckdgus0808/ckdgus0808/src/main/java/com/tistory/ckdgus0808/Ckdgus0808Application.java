32758902584146056c9bd4fea0a4b5c0d4d0c72d
ckdgus08 <ckdgus0808@naver.com>
2020-07-24 10:32:17 +0900

    intelliJ, gradle, sdk, jdk버전 개발환경 셋팅

---
+++ ckdgus0808/ckdgus0808/src/main/java/com/tistory/ckdgus0808/Ckdgus0808Application.java
@@ -0,0 +1,13 @@
+package com.tistory.ckdgus0808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ckdgus0808Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ckdgus0808Application.class, args);
+	}
+
+}
---
+++ ckdgus0808/ckdgus0808/src/test/java/com/tistory/ckdgus0808/Ckdgus0808ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.ckdgus0808;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ckdgus0808ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
